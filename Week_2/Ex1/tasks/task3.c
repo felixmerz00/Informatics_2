bdb4b42b972266f0db83c3392f03e7bfff4f173e
Informatik_2 <felixmerz00@gmail.com>
2022-02-28 16:57:52 +0100

    task3 and task4 completed

--- Week_2/Ex1/tasks/task3.c
+++ Week_2/Ex1/tasks/task3.c
@@ -8,13 +8,51 @@
 #include <stdio.h>
 
 void selectionSort(int A[], int n) {
+	int i;
+	for(i = 0; i < n-1; i++){
+		//find the position of the lowest value
+		int target_pos = i;
+		for(int j = i+1; j < n; j++){
+			if(A[j] < A[target_pos]){
+				target_pos = j;
+			}
+		}
+		//swap the two values
+		int t = A[i];
+		A[i] = A[target_pos];
+		A[target_pos] = t;
+	}
 
+	//print the array
+	for(i = 0; i < n; i++){
+		printf(" %d", A[i]);
+	}
+	printf("\n");
 	return;
 }
 
 void evenOddSelectionSort(int A[], int n){
+	int even_n, i = 0;
+	int odd_n = 0;
+	int E[100], O[100];
 
-	
+	//distribute the numbers in an odd array and an even array and count their length
+	for(i = 0; i < n; i++){
+		if(A[i] % 2 == 0){
+			E[even_n] = A[i];
+			even_n++;
+		}
+		else{
+			O[odd_n] = A[i];
+			odd_n++;
+		}
+	}
+
+	//sort both arrays and print them
+	printf("Sorted even numbers:");
+	selectionSort(E, even_n);
+	printf("Sorted odd numbers:");
+	selectionSort(O, odd_n);
 	return;
 }
 
@@ -22,16 +60,16 @@ int main(int argc, const char * argv[]) {
 	int A[100];
 	int n = 0;
 
+	/*On success, the scanf function returns the number of items of the argument list 
+	successfully read.*/
 	printf("Values of A separated by spaces (non-number to stop):");
-	
 	while (scanf("%d", &A[n]) == 1) {
 		n++;
 	}
-
 	scanf("%*s");
 	
-	//printf("Result: ");
-	//evenOddSelectionSort(A, n);
+	printf("Result:\n");
+	evenOddSelectionSort(A, n);
 }
 
 // Linux, Mac: gcc task4.c -o task4; ./task4
--- Week_2/Ex1/tasks/task4.c
+++ Week_2/Ex1/tasks/task4.c
@@ -16,6 +16,58 @@ int main() {
 		pos++;
 	}
 
+	int length = pos;
+	for(int i = 0; i < length; i++){
+		printf(" %d", timestamps[i]);
+	}
+	printf("\n");
+
+	//insertionsort the array, if an element is equal to another element, dicard it
+	for(int i = 1; i < length; i++){
+		int target = timestamps[i];
+
+		//test if target already exists in sorted section
+		int target_exists = 0;
+		for(int k = 0; k < i; k++){
+			if(timestamps[k] == target){
+				target_exists = 1;
+			}
+		}
+
+		//if target exists, add a -1 at the end of the sorted section
+		if(target_exists){
+			timestamps[i] = -1;
+		}
+		//if target doesn't exist yet in sorted section, insert it with insertion sort
+		else{
+			int j = i-1;
+			while((timestamps[j] == -1 || target < timestamps[j]) && j >= 0){
+				timestamps[j+1] = timestamps[j];
+				j--;
+			}
+			timestamps[j+1] = target;
+		}
+
+	}
+
+	//find biggest gap
+	int biggest_gap = timestamps[1] - timestamps[0];
+	int most_recent_timestamp = timestamps[0];
+	for(int i = 1; i < length && timestamps[i] != -1; i++){
+		int current_gap = timestamps[i+1]-timestamps[i];
+		if(current_gap > biggest_gap){
+			biggest_gap = current_gap;
+		}
+		most_recent_timestamp = timestamps[i];
+	}
+
+	for(int i = 0; i < length; i++){
+		printf(" %d", timestamps[i]);
+	}
+	printf("\n");
+
+	//return oldest, newest timestamp and biggest gap
+	printf("Oldest timestamp: %d, most recent timestamp: %d, biggest gap: %d\n", timestamps[0], most_recent_timestamp, biggest_gap);
 
 	return 0;
 }
