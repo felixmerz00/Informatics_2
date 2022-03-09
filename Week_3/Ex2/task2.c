ee2a26996bdcc739c18d25bb67c67f234ed73545
Felix Merz <felixmerz@MacBook-Felix.local>
2022-03-09 10:29:00 +0100

    Task 2 completed

--- Week_3/Ex2/task2.c
+++ Week_3/Ex2/task2.c
@@ -11,8 +11,20 @@
 const int MAX_LENGTH = 1000;
 
 // TODO: your implementation
-int find_second_smallest(int A[], int i)
+int find_second_smallest(int A[], int i, int length)
 {
+	int count = 0;
+	for(int j = 0; j < length; j++){
+		if(A[j] < A[i]){
+			count++;
+		}
+	}
+	//base case
+	if(count == 1){return A[i];}
+	//if not, try for element at postion i+1
+	else{
+		return find_second_smallest(A, i+1, length);
+	}
 	return 0;
 }
 
@@ -26,6 +38,8 @@ int main() {
 	// variable pos will contain number of integers read in from user
 
 	// TODO: your implementation
+	int second_smallest_integer = find_second_smallest(arr, 0, pos);
+	printf("The second smallest integer is: %d\n", second_smallest_integer);
 
 	return 0;
 }
