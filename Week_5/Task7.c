8305f1386dbe9d8ba95657aea6451793a3b25c39
Informatik_2 <felixmerz00@gmail.com>
2022-04-03 13:57:55 +0200

    Task 7.a.iii completed. Lists with size 6 or smaller are now sorted with insertion sort.

--- Week_5/Task7.c
+++ Week_5/Task7.c
@@ -2,6 +2,34 @@
 #include <math.h>
 #include <stdlib.h>
 
+void insertionSort(int *A, int l, int r, int direction)
+{
+    int x, j, i;
+    if(direction == 1){
+        for(i = l + 1; i <= r; i++){
+            j = i-1;
+            x = A[i];
+            while(j >= l && x < A[j]){
+                A[j+1] = A[j];
+                j--;
+            }
+            A[j+1] = x;
+        }    
+    }
+    else{
+        for(i = l + 1; i <= r; i++){
+            j = i-1;
+            x = A[i];
+            while(j >= l && x > A[j]){
+                A[j+1] = A[j];
+                j--;
+            }
+            A[j+1] = x;
+        }  
+    }
+    
+}
+
 //tests weather an array is sorted in the given direction (ascending: 1, descending: -1)
 //returns 1 for sorted, -1 for not sorted
 int array_sorted(int *A, int length, int direction){
@@ -69,11 +97,15 @@ void merge(int *A, int l, int r, int m, int direction)
 
 void mergeSort(int *A, int l, int r, int direction)
 {
-    if(l < r){
+    //use mergesort if the array has a length greater then 6, else use insertion sort
+    if(r-l+1 > 6){
         int m = floor((l+r)/2);
         mergeSort(A, l, m, direction);
         mergeSort(A, m+1, r, direction);
         merge(A, l, r, m, direction);
+    }
+    else{
+        insertionSort(A, l, r, direction);
     }
 }
 
@@ -96,8 +128,8 @@ int main()
     }
 
     //allocate memory for the array
-    //int A[8] = {5, 2, 4, 7, 1, 3, 6, 2};
-    int A[8] = {7, 6, 5, 4, 3, 2, 2, 1};
+    int A[8] = {5, 2, 4, 7, 1, 3, 6, 2};
+    //int A[8] = {7, 6, 5, 4, 3, 2, 2, 1};
     int length = 8;
     int *pA;
     pA = (int*)malloc(8*sizeof(int));
@@ -109,5 +141,29 @@ int main()
     if(array_sorted(pA, length, direction) == -1){
         mergeSort(pA, 0, length-1, direction);
     }
-    print_Array(pA, length);    
+    print_Array(pA, length);  
+
+    /*test for insertion sort method
+    int *pB;
+    pB = (int*)malloc(8*sizeof(int));
+    for(int i = 0; i < length; i++){
+        pB[i] = A[i];
+    }
+    //partial array ascending
+    printf("Insertionsort ascending from index 2 to 6:\n");
+    insertionSort(pB, 2, 6, 1);
+    print_Array(pB, length);
+    //partial array descending
+    printf("Insertionsort descending from index 1 to 4:\n");
+    insertionSort(pB, 1, 4, -1);
+    print_Array(pB, length);
+    //complete array ascending
+    printf("Insertionsort ascending:\n");
+    insertionSort(pB, 0, length-1, 1);
+    print_Array(pB, length);
+    //complete array descending
+    printf("Insertionsort descending:\n");
+    insertionSort(pB, 0, length-1, -1);
+    print_Array(pB, length);*/
+
 }
