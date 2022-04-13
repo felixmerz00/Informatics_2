f54b6b6de4caddf211ea7b0e14b125712fe324d8
Felix Merz <felixmerz@MacBook-Felix.local>
2022-04-13 11:43:44 +0200

    Complexity improved by 1*n

--- Week_8/Task3.c
+++ Week_8/Task3.c
@@ -5,8 +5,8 @@ void get_span(int data[], int length_data, int* spans)
 {
     int i, j;
     for(i = 0; i < length_data; i++){
-        int count = 0;
-        for(j = i; j >= 0 && data[j] <= data[i]; j--){
+        int count = 1;
+        for(j = i-1; j >= 0 && data[j] <= data[i]; j--){
             count++;
         }
         spans[i] = count;
@@ -32,3 +32,9 @@ int main()
     print_array(spans, length_data);
     return 0;
 }
+
+/*Task3
+Consider an array A[0..n − 1] of n integers. The span s(A, i) of array element A[i] is the maximum number of 
+consecutive elements A[j] immediately preceding A[i] and such that A[j] ≤ A[i]. For example, array A = [6,3,4,5,2] 
+has the spans s(A,0) = 1, s(A,1) = 1, s(A,2) = 2, s(A,3) = 3 and s(A, 4) = 1 which can be written as an array as 
+follows: s(A) = [1, 1, 2, 3, 1].*/
