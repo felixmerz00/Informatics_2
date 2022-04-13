d01cda3ad3881d02e3fd9e0d4e07d803ba09322a
Felix Merz <felixmerz@MacBook-Felix.local>
2022-04-13 09:12:52 +0200

    implemented solutions Exercise SL01-4

--- Week_1/Recursion_Odd_Even.c
+++ Week_1/Recursion_Odd_Even.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int even(int n);
+int even2(int n);
+
+/*Odd2 and even2 are based on the given solution from slide 65/82, p.148 from the slide deck.*/
+int odd2(int n)
+{
+    return n > 0 && even2(n-1);
+}
+
+int even2(int n)
+{
+    return (n == 0 || odd2(n-1));
+}
 
 int odd(int n)
 {
@@ -24,9 +36,12 @@ int even(int n)
 
 int main()
 {
-    int n, is_even, is_odd;
+    int n;
     printf("Enter a number: ");
     scanf("%d", &n);
+
+    /*uncomment this section if you want to use even(), odd() instead of even2(), odd2()
+    int is_even, is_odd;
     if(n < 0){
         printf("The number must be greater or equal to zero.\n");
         return 0;
@@ -38,6 +53,19 @@ int main()
     }
     else{
         printf("The number %d is odd.\n", n);
+    }*/
+
+
+    //odd2 and even2 are based on the given solution from slide 65/82, p.148 from the slide deck.
+    if(even2(n)){
+        printf("The number %d is even.\n", n);
     }
+    else if(odd2(n)){
+        printf("The number %d is odd.\n", n);
+    }
+    else{
+        printf("The number %d is invalid\n", n);
+    }
+
     return 0;
 }
